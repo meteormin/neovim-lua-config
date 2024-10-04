8b9298b017f54d85e1e533dbd3862b3fa4f7a6e3
SeongMin Yoo <42951596+meteormin@users.noreply.github.com>
2024-10-04 12:51:49 +0900

    Update nvim-cmp.lua

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -35,7 +35,7 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.corfirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
