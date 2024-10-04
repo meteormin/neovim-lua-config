070dce08f809d44eb49b34dde073b0d7c355998c
SeongMin Yoo <42951596+meteormin@users.noreply.github.com>
2024-10-04 12:05:27 +0900

    Update alpha.lua

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -7,6 +7,16 @@ return {
 	config = function()
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.dashboard")
+
+		 -- set menu items
+	        dashboard.section.buttons.val = {
+	            dashboard.button( "n", "  > New file" , ":ene <BAR> startinsert <CR>"),
+	            dashboard.button( "f", "  > Find file", ":Telescope find_files<CR>"),
+	            dashboard.button( "w", "  > Find Word", ":Telescope live_grep <CR>"),
+	            dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+	            dashboard.button( "q", "  > Quit", ":qa<CR>"),
+	        }
+	
 		alpha.setup(dashboard.opts)
 	end,
 }
