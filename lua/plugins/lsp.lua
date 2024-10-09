2f1fae68428d13a463cd26b204a20900eb8b1fd5
meteormin <miniyu97@gmail.com>
2024-10-09 23:34:13 +0900

    add: lsp shortcut

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,42 +1,50 @@
-local keyMapper = require('utils.keyMapper').mapKey
+local keyMapper = require("utils.keyMapper").mapKey
 
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require('mason').setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require('mason-lspconfig').setup({
-        ensure_installed = {
-          "clangd", "lua_ls", "eslint", "gopls",
-          "jsonls", "marksman", "yamlls"
-        }
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.eslint.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.marksman.setup({})
-      lspconfig.yamlls.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"clangd",
+					"lua_ls",
+					"eslint",
+					"gopls",
+					"jsonls",
+					"marksman",
+					"yamlls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.gopls.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.eslint.setup({})
+			lspconfig.jsonls.setup({})
+			lspconfig.marksman.setup({})
+			lspconfig.yamlls.setup({})
 
-    --   vim.lsp.buf.hover
-    --   vim.lsp.buf.definition
-    --   vim.lsp.buf.code_action
-    keyMapper('K', vim.lsp.buf.hover)
-    keyMapper('gd', vim.lsp.buf.definition)
-    keyMapper('<leader>ca', vim.lsp.buf.code_action)
-
-    end,
-  }
+			--  vim.lsp.buf.hover: hover documentation
+			--  vim.lsp.buf.definition: go to definition
+			--  vim.lsp.buf.code_action: code action
+			--  vim.lsp.buf.references: show all references
+			keyMapper("K", vim.lsp.buf.hover)
+			keyMapper("gd", vim.lsp.buf.definition)
+			keyMapper("<leader>ca", vim.lsp.buf.code_action)
+			keyMapper("gr", vim.lsp.buf.references)
+			keyMapper("<C-k>", vim.lsp.buf.signature_help)
+			keyMapper("<leader>f", vim.lsp.buf.format)
+		end,
+	},
 }
